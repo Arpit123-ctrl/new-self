9fcb72f0a219fa62791b6186c3ed4f870a5bf194
Arpit <Arpit@2005>
2025-11-16 00:35:13 +0530

    chages in main2

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-let url="https://quote-garden.onrender.com/api/v3/quotes";
-let btn=document.querySelector("button");
-btn.addEventListener("click",async ()=>{
-    let ans=await axios.get(url);
-
-  console.log(ans.data);
-});
+let name="Arpit";
